03559585ac274da48247e94eee5578b8f06dc839
Aktilek <janybaevaktilek@gmail.com>
2023-04-12 22:32:55 +0600

    add new styles

---
+++ src/component/UI/Input/Input.stories.tsx
@@ -0,0 +1,32 @@
+import { StoryFn, Meta } from '@storybook/react'
+import Input from './Input'
+import { action } from '@storybook/addon-actions'
+
+export default {
+   title: 'UI/Input',
+   component: Input,
+   args: {},
+   argTypes: {
+      onChange: { type: 'string' },
+   },
+} as Meta
+
+const Template: StoryFn<typeof Input> = (args) => (
+   <Input {...args} onChange={(e) => action('Input')(e.target.value)} />
+)
+
+export const Outlined = Template.bind({})
+
+Outlined.args = {
+   label: 'Outlined',
+   fullWidth: false,
+   error: false,
+   disabled: false,
+}
+
+Outlined.argTypes = {
+   variant: {
+      control: 'inline-radio',
+      options: ['filled', 'outlined', 'standard'],
+   },
+}
--- src/component/UI/theme/index.tsx
+++ src/component/UI/theme/index.tsx
@@ -38,28 +38,7 @@ const Theme: FC<ThemeProps> = ({ children }) => {
                },
             },
          },
-         MuiTextField: {
-            styleOverrides: {
-               root: {
-                  '& .MuiInputBase-input': {
-                     color: 'black',
-                  },
-                  '&:hover': {
-                     cursor: 'pointer',
-                     outline: '0.5px solid #cacaca',
-                     borderRadius: '4px',
-                  },
-               },
-            },
          },
-         MuiInputLabel: {
-            styleOverrides: {
-               root: {
-                  color: '#cacaca',
-               },
-            },
-         },
-      },
       palette: {
          primary: {
             main: '#3A10E5',
