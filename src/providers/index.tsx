803c1f1137fd4e9d789d889750bb576b78c577c2
Nurtimax <maksatbeknurtilek@gmail.com>
2023-05-17 23:40:18 +0600

    fix it some header feature

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@mui/material';
 import { FC, ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -12,6 +13,7 @@ interface IProvidersProps {
 const Providers: FC<IProvidersProps> = ({ children }) => {
    return (
       <>
+         <CssBaseline />
          <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                <Theme>{children}</Theme>
