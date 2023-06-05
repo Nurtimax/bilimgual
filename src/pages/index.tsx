647f3f0d488625e43366b990d1fb6b8c0a400d6d
Nurtimax <maksatbeknurtilek@gmail.com>
2023-06-05 16:33:35 +0600

    change default pistion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,11 @@
-// import MainLayout from '../layout';
-
-import { Box } from '@mui/material';
-
-import { Typography } from '../component/UI';
-
-type ITypographyVariantsType =
-   | 'h1'
-   | 'h2'
-   | 'h3'
-   | 'h4'
-   | 'h5'
-   | 'h6'
-   | 'body1'
-   | 'body2'
-   | 'body3'
-   | 'body4'
-   | 'bodySmall'
-   | 'subtitle1'
-   | 'subtitle2'
-   | 'caption'
-   | 'button'
-   | 'overline'
-   | 'inherit'
-   | 'bold'
-   | 'medium'
-   | 'regular';
-
-const typographyVariants: ITypographyVariantsType[] = [
-   'h1',
-   'h2',
-   'h3',
-   'h4',
-   'h5',
-   'h6',
-   'body1',
-   'body2',
-   'body3',
-   'body4',
-   'bodySmall',
-   'subtitle1',
-   'subtitle2',
-   'caption',
-   'button',
-   'overline',
-   'inherit',
-   'bold',
-   'medium',
-   'regular'
-];
+import MainLayout from '../layout';
 
 export default function Home() {
    return (
       <>
-         {/* <MainLayout></MainLayout> */}
-         {typographyVariants.map((item) => (
-            <Box key={item}>
-               <Typography variant={item}>Typography variant: {item}</Typography>
-            </Box>
-         ))}
+         <MainLayout>
+            <h1>Hello Bilingual</h1>
+         </MainLayout>
       </>
    );
 }
