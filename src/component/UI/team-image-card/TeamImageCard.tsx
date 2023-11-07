55dbe4a434af88b9efb79787593c1b541fc18ce0
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-07 14:47:33 +0600

    create out team

--- src/component/UI/team-image-card/TeamImageCard.tsx
+++ src/component/UI/team-image-card/TeamImageCard.tsx
@@ -5,13 +5,19 @@ import { ITeamImageCard } from '../../../types/team';
 
 const TeamImageCard: FC<ITeamImageCard> = ({ borderRadius, figCaption, name, staticImage }) => {
    return (
-      <Card elevation={0}>
-         <CardMedia component="img" height="180" sx={{ borderRadius }} image={staticImage} alt="Paella dish" />
+      <Card elevation={0} sx={{}}>
+         <CardMedia
+            component="img"
+            height="250"
+            sx={{ borderRadius, objectFit: 'fill', width: '190px' }}
+            image={staticImage}
+            alt="Paella dish"
+         />
          <CardContent>
             <Typography textAlign="center" variant="h6">
                {name}
             </Typography>
-            <Typography textAlign="center" variant="body4">
+            <Typography textAlign="center" variant="bodySmall">
                {figCaption}
             </Typography>
          </CardContent>
--- src/component/our-team/ImageList.tsx
+++ src/component/our-team/ImageList.tsx
@@ -7,15 +7,71 @@ import { ITeamImageCard } from '../../types/team';
 const teamList: ITeamImageCard[] = [
    {
       id: 1,
-      name: 'Aktilek',
-      figCaption: 'Bilingaula’s frontend developer',
-      staticImage: '',
-      borderRadius: '',
+      name: 'Albina',
+      figCaption: 'Frontend developer',
+      staticImage: '/static/images/albina.jpg',
+      borderRadius: '1px 1px 1px 20px',
       socials: [
          {
             icon: <></>,
             id: 1,
-            link: 'https://github.com/AktilekVIP'
+            link: 'https://github.com/AzamatovaAlbina'
+         }
+      ]
+   },
+   {
+      id: 2,
+      name: 'Asatbek',
+      figCaption: 'Frontend developer',
+      staticImage: '/static/images/asatbek.jpg',
+      borderRadius: '1px 1px 1px 20px',
+      socials: [
+         {
+            icon: <></>,
+            id: 1,
+            link: 'https://github.com/Asatbek1'
+         }
+      ]
+   },
+   {
+      id: 3,
+      name: 'Abdumalik',
+      figCaption: 'Frontend developer',
+      staticImage: '/static/images/malik.jpg',
+      borderRadius: '1px 1px 1px 20px',
+      socials: [
+         {
+            icon: <></>,
+            id: 1,
+            link: 'https://github.com/kaamilov'
+         }
+      ]
+   },
+   {
+      id: 4,
+      name: 'Nurtilek',
+      figCaption: 'Frontend developer',
+      staticImage: '/static/images/nurtilek.jpg',
+      borderRadius: '1px 1px 1px 20px',
+      socials: [
+         {
+            icon: <></>,
+            id: 1,
+            link: 'https://github.com/Nurtimax'
+         }
+      ]
+   },
+   {
+      id: 5,
+      name: 'Saltanat',
+      figCaption: 'Frontend developer',
+      staticImage: '/static/images/saltanat.jpg',
+      borderRadius: '1px 1px 1px 20px',
+      socials: [
+         {
+            icon: <></>,
+            id: 1,
+            link: 'https://github.com/saltanatzakypova'
          }
       ]
    }
@@ -23,9 +79,9 @@ const teamList: ITeamImageCard[] = [
 
 const ImageList = () => {
    return (
-      <Grid container>
+      <Grid container py={6}>
          {teamList.map((team) => (
-            <Grid key={team.id}>
+            <Grid key={team.id} xs={2.4} sx={{ placeItems: 'center', display: 'grid' }}>
                <TeamImageCard {...team} />
             </Grid>
          ))}
