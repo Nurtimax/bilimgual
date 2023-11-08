c6e59ef21c493843651078c5c6b3345022e7644d
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-08 13:26:38 +0600

    do private sign in

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,13 +1,24 @@
 import { Box, Container, styled } from '@mui/material';
 import React, { FC } from 'react';
+import { useRouter } from 'next/router';
 
 import MainSignIn from '../component/sign-in';
+import { useAppSelector } from '../store/hooks';
+import { authSelector } from '../store/slices/authentication-slice';
 
 const StyledSignIn = styled(Box)`
    background: #6b0fa9;
 `;
 
 const SignIn: FC = () => {
+   const { fields } = useAppSelector((state) => authSelector(state));
+   const { replace } = useRouter();
+
+   if (fields.emailVerified) {
+      replace('/');
+      return <></>;
+   }
+
    return (
       <StyledSignIn>
          <Container>
