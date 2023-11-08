1c6f85fb992115dab169f85a668d1d9f34aafa7c
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-08 21:33:22 +0600

    provide formik

--- src/component/sign-up/Forms.tsx
+++ src/component/sign-up/Forms.tsx
@@ -1,4 +1,5 @@
 import { Button, TextField, styled } from '@mui/material';
+import { FormikHelpers, useFormik } from 'formik';
 import React from 'react';
 
 const StyledForms = styled('form')`
@@ -6,15 +7,65 @@ const StyledForms = styled('form')`
    gap: 20px;
 `;
 
-const Forms = () => {
-   return (
-      <StyledForms>
-         <TextField fullWidth label="First name" />
-         <TextField fullWidth label="Last name" />
-         <TextField fullWidth label="Email" />
-         <TextField fullWidth label="Password" />
+interface ISignUpValues {
+   firstName: string;
+   lastName: string;
+   email: string;
+   password: string;
+}
 
-         <Button variant="login" fullWidth>
+const onSubmit = (values: ISignUpValues, formikHelpers: FormikHelpers<ISignUpValues>) => {
+   console.log(values);
+   formikHelpers.resetForm();
+};
+
+const Forms = () => {
+   const { values, errors, handleChange, handleSubmit } = useFormik({
+      initialValues: {
+         email: '',
+         firstName: '',
+         lastName: '',
+         password: ''
+      },
+      onSubmit
+   });
+
+   return (
+      <StyledForms onSubmit={handleSubmit}>
+         <TextField
+            fullWidth
+            onChange={handleChange}
+            value={values.firstName}
+            name="firstName"
+            label="First name"
+            helperText={errors.firstName}
+         />
+         <TextField
+            fullWidth
+            onChange={handleChange}
+            value={values.lastName}
+            name="lastName"
+            label="Last name"
+            helperText={errors.lastName}
+         />
+         <TextField
+            fullWidth
+            onChange={handleChange}
+            value={values.email}
+            name="email"
+            label="Email"
+            helperText={errors.email}
+         />
+         <TextField
+            fullWidth
+            onChange={handleChange}
+            value={values.password}
+            name="password"
+            label="Password"
+            helperText={errors.password}
+         />
+
+         <Button variant="login" type="submit" fullWidth>
             SIGN UP
          </Button>
       </StyledForms>
