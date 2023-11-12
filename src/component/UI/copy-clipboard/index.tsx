a8a31ff95b3ca29be0ddc2029f03b09b3b52a168
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-12 22:22:00 +0600

    remove copied function

--- src/component/UI/copy-clipboard/index.tsx
+++ src/component/UI/copy-clipboard/index.tsx
@@ -1,8 +1,7 @@
 import { IconButton, Stack } from '@mui/material';
-import React, { FC, memo, useEffect, useState } from 'react';
+import React, { FC, memo } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
 interface ICopyClipboard {
    type: string;
@@ -10,33 +9,14 @@ interface ICopyClipboard {
 }
 
 const CopyClipboard: FC<ICopyClipboard> = memo(({ text, type }) => {
-   const [copied, setCopied] = useState(false);
-
-   useEffect(() => {
-      const handleGetClipboard = async () => {
-         const textValue = await window.navigator.clipboard.readText();
-
-         if (text === textValue) {
-            setCopied(true);
-         }
-      };
-      handleGetClipboard();
-   }, [text]);
-
    return (
       <Stack direction="row" alignItems="center">
          {type}: <strong>{text}</strong>
-         {copied ? (
+         <CopyToClipboard text={text}>
             <IconButton size="small">
-               <CheckBoxIcon sx={{ width: 15, height: 15 }} />
+               <ContentCopyIcon sx={{ width: 15, height: 15 }} />
             </IconButton>
-         ) : (
-            <CopyToClipboard text={text}>
-               <IconButton size="small" onClick={() => setCopied(true)}>
-                  <ContentCopyIcon sx={{ width: 15, height: 15 }} />
-               </IconButton>
-            </CopyToClipboard>
-         )}
+         </CopyToClipboard>
       </Stack>
    );
 });
