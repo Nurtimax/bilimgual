99bf31373dbdf518c44706a341b261d44e2ff73a
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-17 20:30:42 +0600

    remove dummy our team data

--- src/component/our-team/ImageList.tsx
+++ src/component/our-team/ImageList.tsx
@@ -1,131 +1,17 @@
 import { Grid } from '@mui/material';
-import React from 'react';
+import React, { FC } from 'react';
 
 import TeamImageCard from '../UI/team-image-card/TeamImageCard';
 import { ITeamImageCard } from '../../types/team';
 
-const teamList: ITeamImageCard[] = [
-   {
-      id: 1,
-      fullName: 'Albina',
-      staticImage: '/static/images/albina.jpg',
-      borderRadius: '1px 1px 1px 20px',
-      socials: [
-         {
-            icon: 'github',
-            id: 'github',
-            link: 'https://github.com/AzamatovaAlbina'
-         }
-      ],
-      address: '',
-      city: '',
-      company: '',
-      country: '',
-      email: '',
-      phoneNumber: '',
-      role: '',
-      stateRegion: '',
-      zipCode: '',
-      position: ''
-   },
-   {
-      id: 2,
-      fullName: 'Asatbek',
-      staticImage: '/static/images/asatbek.jpg',
-      borderRadius: '1px 1px 1px 20px',
-      socials: [
-         {
-            icon: 'github',
-            id: 'github',
-            link: 'https://github.com/Asatbek1'
-         }
-      ],
-      address: '',
-      city: '',
-      company: '',
-      country: '',
-      email: '',
-      phoneNumber: '',
-      role: '',
-      stateRegion: '',
-      zipCode: '',
-      position: ''
-   },
-   {
-      id: 3,
-      fullName: 'Abdumalik',
-      staticImage: '/static/images/malik.jpg',
-      borderRadius: '1px 1px 1px 20px',
-      socials: [
-         {
-            icon: 'github',
-            id: 'github',
-            link: 'https://github.com/kaamilov'
-         }
-      ],
-      address: '',
-      city: '',
-      company: '',
-      country: '',
-      email: '',
-      phoneNumber: '',
-      role: '',
-      stateRegion: '',
-      zipCode: '',
-      position: ''
-   },
-   {
-      id: 4,
-      fullName: 'Nurtilek',
-      staticImage: '/static/images/nurtilek.jpg',
-      borderRadius: '1px 1px 1px 20px',
-      socials: [
-         {
-            icon: 'github',
-            id: 'github',
-            link: 'https://github.com/Nurtimax'
-         }
-      ],
-      address: '',
-      city: '',
-      company: '',
-      country: '',
-      email: '',
-      phoneNumber: '',
-      role: '',
-      stateRegion: '',
-      zipCode: '',
-      position: ''
-   },
-   {
-      id: 5,
-      fullName: 'Saltanat',
-      staticImage: '/static/images/saltanat.jpg',
-      borderRadius: '1px 1px 1px 20px',
-      socials: [
-         {
-            icon: 'github',
-            id: 'github',
-            link: 'https://github.com/saltanatzakypova'
-         }
-      ],
-      address: '',
-      city: '',
-      company: '',
-      country: '',
-      email: '',
-      phoneNumber: '',
-      role: '',
-      stateRegion: '',
-      zipCode: '',
-      position: ''
-   }
-];
+interface ImageListProps {
+   teams: ITeamImageCard[];
+}
 
-const ImageList = () => {
+const ImageList: FC<ImageListProps> = ({ teams }) => {
    return (
       <Grid container py={6} spacing={1} rowSpacing={5}>
-         {teamList.map((team) => (
+         {teams.map((team) => (
             <Grid
                item
                key={team.id}
--- src/component/our-team/index.tsx
+++ src/component/our-team/index.tsx
@@ -1,16 +1,30 @@
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import { Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+
+import { getOurTeamThunk, selectorOurTeam } from '../../store/slices/our-team';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 
 import ImageList from './ImageList';
 
 const MainOutTeam = () => {
+   const dispatch = useAppDispatch();
+   const { loading, teams } = useAppSelector(selectorOurTeam);
+
+   useEffect(() => {
+      dispatch(getOurTeamThunk());
+   }, [dispatch]);
+
+   if (!teams.length) {
+      return null;
+   }
+
    return (
-      <Box>
+      <>
          <Typography variant="h3" component="h2" textAlign="center" color="#3752B4">
             Our Team
          </Typography>
-         <ImageList />
-      </Box>
+         {loading ? <h1>Loading...</h1> : <ImageList teams={teams} />}
+      </>
    );
 };
 
