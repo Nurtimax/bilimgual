06bdf6118c2089b25ccf07cb90db5a8b1e760d50
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-27 19:38:34 +0600

    remove unused txt files and remove dummy data

--- src/component/footer/main.tsx
+++ src/component/footer/main.tsx
@@ -1,16 +1,38 @@
 import { Accordion, AccordionDetails, AccordionSummary, Box, Typography, styled, IconButton } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import { toast } from 'react-toastify';
 
 import { BilingualFullIcon } from '../../assets/icons/BilingualFullIcon';
 import { SOCIAL_ICONS } from '../../utils/constants/icons';
-import accordions from '../../utils/constants/accordions';
+import axiosInctanse from '../../utils/helpers/axiosInstance';
+import { IFAQValues } from '../admin/footer';
 
 const StyledMainFooter = styled(Box)`
    padding: 2rem 0 0;
 `;
 
 const MainFooter = () => {
+   const [accordions, setAccordions] = useState<IFAQValues[]>([]);
+
+   const getAccordions = async () => {
+      try {
+         const response = await axiosInctanse.get(`footer_faq/users.json`);
+
+         const accordions = Array.isArray(response.data) ? response.data : [];
+
+         setAccordions(accordions);
+      } catch (error) {
+         if (error instanceof Error) {
+            toast.error(error.message);
+         }
+      }
+   };
+
+   useEffect(() => {
+      getAccordions();
+   }, []);
+
    return (
       <StyledMainFooter>
          <Typography variant="h5" color="inherit">
@@ -20,9 +42,9 @@ const MainFooter = () => {
             {accordions.map((accordion) => (
                <Accordion key={accordion.id}>
                   <AccordionSummary expandIcon={<AddIcon sx={{ color: 'white' }} />}>
-                     {accordion.summary}{' '}
+                     {accordion.question}{' '}
                   </AccordionSummary>
-                  <AccordionDetails sx={{ color: '#fff' }}>{accordion.details}</AccordionDetails>
+                  <AccordionDetails sx={{ color: '#fff' }}>{accordion.answer}</AccordionDetails>
                </Accordion>
             ))}
          </Box>
