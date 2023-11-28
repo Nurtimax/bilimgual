f01d343807f7f054fc04adc9512f53233352d766
Nurtimax <maksatbeknurtilek@gmail.com>
2023-11-28 22:12:18 +0600

    add field

--- src/utils/constants/_api.ts
+++ src/utils/constants/_api.ts
@@ -1,3 +1,4 @@
 /* eslint-disable no-undef */
 
-export const FIREBASE_REALTIME_URL = process.env.NEXT_PUBLIC_FIREBASE_REALTIME_URL || '';
+export const FIREBASE_REALTIME_URL =
+   process.env.NEXT_PUBLIC_FIREBASE_REALTIME_URL || process.env.REACT_APP_FIREBASE_REALTIME_URL || '';
