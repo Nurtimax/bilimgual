46ad7f1f6975596b62fa4d2f188800465c418c92
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 16:27:16 +0600

    add gtag in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import React from 'react';
 
 export default function Document() {
    return (
       <Html lang="en">
          <Head />
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V4WF9Z7HM5" />
+         <Script>
+            {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V4WF9Z7HM5');`}
+         </Script>
          <body>
             <Main />
             <NextScript />
