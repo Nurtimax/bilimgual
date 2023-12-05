36c56599bf857f6d3859a11c13858f284b753cb2
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 20:18:46 +0600

    add matrix

--- src/component/page/index.tsx
+++ src/component/page/index.tsx
@@ -37,53 +37,6 @@ const Page: FC<IPage> = memo(({ children, title, canonical, description }) => {
             )}
          </Helmet>
          {children}
-         <script
-            dangerouslySetInnerHTML={{
-               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-MJN54M5X');`
-            }}
-         ></script>
-         <noscript>
-            <iframe
-               src="https://www.googletagmanager.com/ns.html?id=GTM-MJN54M5X"
-               height="0"
-               width="0"
-               style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-         </noscript>
-         {process.env.NODE_ENV === 'production' && (
-            <>
-               <script
-                  type="text/javascript"
-                  dangerouslySetInnerHTML={{
-                     __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-               m[i].l=1*new Date();
-               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-               ym(95781166, "init", {
-                  clickmap:true,
-                  trackLinks:true,
-                  accurateTrackBounce:true,
-                  webvisor:true
-               });`
-                  }}
-               ></script>
-               <noscript>
-                  <div>
-                     <img
-                        src="https://mc.yandex.ru/watch/95781166"
-                        style={{ position: 'absolute', left: '-99999px' }}
-                        alt=""
-                     />
-                  </div>
-               </noscript>
-            </>
-         )}
       </Box>
    );
 });
