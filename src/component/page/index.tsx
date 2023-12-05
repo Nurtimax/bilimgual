40090525b55eab4fa46b3f57f3960c85469cd163
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 17:17:57 +0600

    add helmet script

--- src/component/page/index.tsx
+++ src/component/page/index.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import Script from 'next/script';
 import React, { FC, ReactNode, memo } from 'react';
 import { Helmet } from 'react-helmet-async';
 
@@ -14,8 +15,21 @@ const Page: FC<IPage> = memo(({ children, title, canonical, description }) => {
       <Box>
          <Helmet>
             {title && <title>{title}</title>}
+
             {canonical && <link rel="canonical" href={window.location.origin + canonical} />}
+
             {description && <meta name="description" content={description} />}
+
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V4WF9Z7HM5" />
+            <Script
+               dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               
+               gtag('config', 'G-V4WF9Z7HM5');`
+               }}
+            />
          </Helmet>
          {children}
       </Box>
