803149554c37f8d0633d2f696f969e26aaea9e2e
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 17:14:10 +0600

    head children script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,18 @@ import React from 'react';
 export default function Document() {
    return (
       <Html lang="en">
-         <Head />
-
-         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V4WF9Z7HM5" />
-         <Script
-            dangerouslySetInnerHTML={{
-               __html: `window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-V4WF9Z7HM5');`
-            }}
-         />
+         <Head>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-V4WF9Z7HM5" />
+            <Script
+               dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+               
+               gtag('config', 'G-V4WF9Z7HM5');`
+               }}
+            />
+         </Head>
          <body>
             <Main />
             <NextScript />
