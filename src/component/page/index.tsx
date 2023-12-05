a5d8811491d90b141e9349401a611e441e1a83c7
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 17:53:33 +0600

    fix it infinity render

--- src/component/page/index.tsx
+++ src/component/page/index.tsx
@@ -38,17 +38,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-MJN54M5X');`
                }}
             ></script>
-
-            <noscript>
-               <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=GTM-MJN54M5X"
-                  height="0"
-                  width="0"
-                  style={{ display: 'none', visibility: 'hidden' }}
-               ></iframe>
-            </noscript>
          </Helmet>
          {children}
+         <noscript>
+            <iframe
+               src="https://www.googletagmanager.com/ns.html?id=GTM-MJN54M5X"
+               height="0"
+               width="0"
+               style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+         </noscript>
       </Box>
    );
 });
