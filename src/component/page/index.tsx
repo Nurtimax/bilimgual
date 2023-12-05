708c241e34c5edc9e82418e693b4bde33a73bf9a
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-05 19:58:07 +0600

    provided yandex metrica

--- src/component/page/index.tsx
+++ src/component/page/index.tsx
@@ -49,6 +49,32 @@ const Page: FC<IPage> = memo(({ children, title, canonical, description }) => {
                style={{ display: 'none', visibility: 'hidden' }}
             ></iframe>
          </noscript>
+         <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+               __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+               m[i].l=1*new Date();
+               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+               ym(95781166, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+               });`
+            }}
+         ></script>
+         <noscript>
+            <div>
+               <img
+                  src="https://mc.yandex.ru/watch/95781166"
+                  style={{ position: 'absolute', left: '-99999px' }}
+                  alt=""
+               />
+            </div>
+         </noscript>
       </Box>
    );
 });
