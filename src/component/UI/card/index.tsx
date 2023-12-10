b07828ac6e22851c656450c4b4e2f5d5ba576e20
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-11 03:47:04 +0600

    fix it deploy error

--- src/component/UI/card/index.tsx
+++ src/component/UI/card/index.tsx
@@ -7,11 +7,13 @@ import {
    CardHeader,
    CardHeaderProps,
    CardProps,
-   Card as MuiCard
+   Card as MuiCard,
+   PaperTypeMap
 } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
 
 interface ICard {
-   cardProps?: CardProps & { component?: 'form' | ReactNode };
+   cardProps?: CardProps & { component?: 'form' | ReactNode | OverridableComponent<PaperTypeMap<object, 'div'>> };
    contentProps?: CardContentProps;
    actionProps?: CardActionsProps;
    headerProps?: CardHeaderProps;
