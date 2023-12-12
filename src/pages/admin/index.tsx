a1257637236b874075a6e404ad0c674b6e8b3ddd
Nurtimax <maksatbeknurtilek@gmail.com>
2023-12-12 20:29:12 +0600

    fix it navigate logo

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container } from '@mui/material';
+
+import ToolbarComponent from '../../component/page/ToolbarComponent';
 
 const Admin = () => {
-   return <div>Admin</div>;
+   return (
+      <ToolbarComponent>
+         <Container>Admin</Container>
+      </ToolbarComponent>
+   );
 };
 
 export default Admin;
