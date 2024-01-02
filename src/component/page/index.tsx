d72b5f3087cce56a20df84e2f1fed10a295a0220
Nurtimax <maksatbeknurtilek@gmail.com>
2024-01-02 18:14:04 +0600

    add meta tag yandex

--- src/component/page/index.tsx
+++ src/component/page/index.tsx
@@ -35,6 +35,7 @@ const Page: FC<IPage> = memo(({ children, title, canonical, description }) => {
                </>
             )}
             <meta name="yandex-verification" content="aa6390321201b1cf" />
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
          </Helmet>
          {children}
       </>
